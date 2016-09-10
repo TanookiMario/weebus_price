0c937ddba543b06d7cdd9a05931dcb54f6cea2c1
Chris Geihsler <chris@geihsler.net>
2016-09-10 14:20:57 -0400

    Herokuify

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,10 @@ defmodule WeebusPrice.UserSocket do
   # channel "room:*", WeebusPrice.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket,
+    Phoenix.Transports.WebSocket,
+    timeout: 45_000
+
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
