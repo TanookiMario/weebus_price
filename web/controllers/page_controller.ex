2c51f00a03704bb38331304336d95a1a08d9fb2b
Chris Geihsler <chris@geihsler.net>
2016-09-13 00:17:36 -0400

    Show daily amount to spend.

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,26 @@
 defmodule WeebusPrice.PageController do
   use WeebusPrice.Web, :controller
 
+  alias WeebusPrice.DateMath
+  alias WeebusPrice.Transaction
+
   def index(conn, _params) do
-    render conn, "index.html"
+    day       = DateMath.today
+    first_day = DateMath.first_day_of_month(day)
+    last_day  = DateMath.last_day_of_month(day)
+
+    limit_by_day =
+      Transaction.in_date_range(first_day, last_day)
+      |> Repo.all
+      |> WeebusPrice.DailySpendByPerson.calculate
+      |> WeebusPrice.MonthlyLimit.calculate
+
+    todays_data = for {person, limit} <- limit_by_day, into: %{} do
+      {person, limit[DateMath.today]}
+    end
+
+    conn
+    |> assign(:todays_data, todays_data)
+    |> render("index.html")
   end
 end
--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -1,3 +1,8 @@
 defmodule WeebusPrice.PageView do
   use WeebusPrice.Web, :view
+  alias Decimal, as: D
+
+  def daily_amount(day_data, person) do
+    D.round(day_data[person][:average_to_meet_goal], 2)
+  end
 end
